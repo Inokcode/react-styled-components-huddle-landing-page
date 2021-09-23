e01bb4fe47109f6af8e8ccab5b9192ff4ec22132
Inokcode <regilearn@gmail.com>
2021-09-23 19:46:57 +0530

    Basic setup added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-// import logo from './logo.svg';
-// import './App.css';
+import { ThemeProvider } from 'styled-components';
+import Header from './components/Header';
+import { Container } from './components/styles/Container.styled';
+import GlobalStyles from './components/styles/Global';
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333',
+  },
+};
 
 function App() {
-  return <div className="App">GBU</div>;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Header />
+        <Container></Container>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
